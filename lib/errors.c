10e05a35292d83218638349356151f1112206737
SARAT KUMAR BEHERA <beherasaratkumar@gmail.com>
2019-01-30 09:07:46 +0530

    Add new NTSTATUS codes

--- lib/errors.c
+++ lib/errors.c
@@ -30,26 +30,52 @@ const char *nterror_to_str(uint32_t status) {
                 return "STATUS_PENDING";
         case SMB2_STATUS_NO_MORE_FILES:
                 return "STATUS_NO_MORE_FILES";
+        case SMB2_STATUS_NO_MORE_EAS:
+                return "STATUS_NO_MORE_EAS";
+        case SMB2_STATUS_INVALID_PARAMETER:
+                return "STATUS_INVALID_PARAMETER";
+        case SMB2_STATUS_NO_SUCH_FILE:
+                return "STATUS_NO_SUCH_FILE";
+        case SMB2_STATUS_END_OF_FILE:
+                return "STATUS_END_OF_FILE";
         case SMB2_STATUS_MORE_PROCESSING_REQUIRED:
                 return "STATUS_MORE_PROCESSING_REQUIRED";
         case SMB2_STATUS_ACCESS_DENIED:
                 return "STATUS_ACCESS_DENIED";
+        case SMB2_STATUS_OBJECT_NAME_NOT_FOUND:
+                return "STATUS_OBJECT_NAME_NOT_FOUND";
+        case SMB2_STATUS_OBJECT_NAME_COLLISION:
+                return "STATUS_OBJECT_NAME_COLLISION";
+        case SMB2_STATUS_OBJECT_PATH_NOT_FOUND:
+                return "STATUS_OBJECT_PATH_NOT_FOUND";
+        case SMB2_STATUS_SHARING_VIOLATION:
+                return "STATUS_SHARING_VIOLATION";
+        case SMB2_STATUS_QUOTA_EXCEEDED:
+                return "STATUS_QUOTA_EXCEEDED";
+        case SMB2_STATUS_NO_EAS_ON_FILE:
+                return "STATUS_NO_EAS_ON_FILE";
         case SMB2_STATUS_LOGON_FAILURE:
                 return "STATUS_LOGON_FAILURE";
-        case SMB2_STATUS_NOT_SUPPORTED:
-                return "SMB2_STATUS_NOT_SUPPORTED";
+        case SMB2_STATUS_ALLOTTED_SPACE_EXCEEDED:
+                return "STATUS_ALLOTTED_SPACE_EXCEEDED";
+        case SMB2_STATUS_INSUFFICIENT_RESOURCES:
+                return "STATUS_INSUFFICIENT_RESOURCES";
         case SMB2_STATUS_BAD_NETWORK_NAME:
                 return "STATUS_BAD_NETWORK_NAME";
         case SMB2_STATUS_NOT_A_DIRECTORY:
                 return "STATUS_NOT_A_DIRECTORY";
-        case SMB2_STATUS_INVALID_PARAMETER:
-                return "STATUS_INVALID_PARAMETER";
-        case SMB2_STATUS_END_OF_FILE:
-                return "STATUS_END_OF_FILE";
         case SMB2_STATUS_FILE_CLOSED:
                 return "STATUS_FILE_CLOSED";
-        case SMB2_STATUS_OBJECT_NAME_NOT_FOUND:
-                return "STATUS_OBJECT_NAME_NOT_FOUND";
+        case SMB2_STATUS_NO_MEMORY:
+                return "STATUS_NO_MEMORY";
+        case SMB2_STATUS_INVALID_CONNECTION:
+                return "STATUS_INVALID_CONNECTION";
+        case SMB2_STATUS_CONNECTION_DISCONNECTED:
+                return "STATUS_CONNECTION_DISCONNECTED";
+        case SMB2_STATUS_NO_USER_SESSION_KEY:
+                return "STATUS_NO_USER_SESSION_KEY";
+        case SMB2_STATUS_NOT_SUPPORTED:
+                return "STATUS_NOT_SUPPORTED";
         default:
                 return "Unknown";
         }
